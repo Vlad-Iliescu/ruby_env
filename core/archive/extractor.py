a3dae360a0e79b00c73915046dd3fafead155efd
Vlad-Iliescu <vlad.iliescu.88@gmail.com>
2016-01-21 19:53:34 +0200

    Archive top folder method

--- core/archive/extractor.py
+++ core/archive/extractor.py
@@ -1,11 +1,23 @@
 import os
+import re
+import subprocess
 
 
 class Extractor(object):
     def __init__(self):
-        self.archive_path =  os.path.abspath('../../dist/')
-        self.cmd = '7za.exe l {0} | FINDSTR "[0-9].D....\>" | FIND /V "\\"'
+        self.archive_path = os.path.abspath('../../dist/')
+        self._7zip = path = os.path.abspath('7za.exe')
+        self.cmd = '%s l {0} | FINDSTR "[0-9].D....\>" | FIND /V "\\"' % self._7zip
 
     def extract(self, filename):
-        path = self.archive_path + filename
+        path = os.path.join(self.archive_path, filename)
 
+    def get_archived_folder_name(self, filename):
+        path = os.path.join(self.archive_path, *filename.split('/'))
+        raw_output = subprocess.getoutput(self.cmd.format(path))
+        top_folder = re.sub('\s+', ' ', raw_output).split(' ')[-1]
+        return top_folder
+
+if __name__ == '__main__':
+    ex = Extractor()
+    print(ex.get_archived_folder_name('2.0.0/ruby_2_0_0_p247_x64.7z'))
--- extract.py
+++ extract.py
@@ -1,25 +1,31 @@
 import os
 import re
 
-file = 'dist/2.0.0/ruby_2_0_0_p247_x64.7z'
+file = "D:\\dev\\python\\ruby_env\\dist\\2.0.0\\ruby_2_0_0_p247_x64.7z"
+# file = 'dist/2.0.0/ruby_2_0_0_p247_x64.7z'
 
 import subprocess
 
-
 cmd = '7za.exe l {0} | FINDSTR "[0-9].D....\>" | FIND /V "\\"'.format(file)
+# cmd = ['7za.exe l {0}'.format(file)]
+
+stdoutdata = subprocess.getoutput(cmd)
+print(stdoutdata)
+exit(0)
 
 folder = set()
+print(cmd)
 with subprocess.Popen(cmd, stdout=subprocess.PIPE) as process:
     l = process.stdout.read()
-print(cmd)
+
 print(l)
 exit(0)
 
 process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 last_line = ''
 folder = set()
-for line in iter(process.stdout.readline,''):
-   folder.add(line.rstrip())
+for line in iter(process.stdout.readline, ''):
+    folder.add(line.rstrip())
 
 folder
 exit(0)
@@ -30,7 +36,7 @@ exit(0)
 cmd = '7za.exe x {0} -y -o{1}'.format(file, 'dist')
 print(cmd)
 process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-for line in iter(process.stdout.readline,''):
+for line in iter(process.stdout.readline, ''):
     print(line.rstrip())
 # process.wait()
 print(process.returncode)
